b65d624c27649e1559c2951de9745495827a9c4f
seeandbesharp-code-review <seeandbesharp@gmail.com>
2026-06-17 02:33:22 +0300

    fix: automated server-side review corrections

--- project1/Middlware/ErrorHandlingMiddleware.cs
+++ project1/Middlware/ErrorHandlingMiddleware.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using System.Threading.Tasks;
 
@@ -25,7 +25,9 @@ namespace Api.Middlware
             catch (Exception ex)
             {
                 httpContext.Response.StatusCode = 500;
+                httpContext.Response.ContentType = "application/json";
                 _logger.LogError(ex + " call stack: " + ex.StackTrace);
+                await httpContext.Response.WriteAsync("{\"error\":\"Internal Server Error\"}");
             }
         }
     }
